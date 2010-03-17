5d81800ee147bbda440ec6fd9a9d0f5855154450
Arthur Ulfeldt <arthur@ulfeldt.com>
2010-03-17 16:58:51 -0700

    added xmonad.hs

---
+++ xmonad.hs
@@ -0,0 +1,24 @@
+import XMonad
+import XMonad.Config.Gnome
+import XMonad.Layout.NoBorders
+import XMonad.ManageHook
+import XMonad.Actions.CycleRecentWS
+import qualified Data.Map as M
+
+
+myManageHook :: [ManageHook]
+myManageHook = 
+    [ resource  =? "Do"   --> doIgnore ]
+
+myKeys (XConfig {modMask = modm}) = M.fromList $
+       	 [ ((modm, xK_p), spawn "gnome-do") 
+          ,((modm, xK_Tab), cycleRecentWS [xK_Alt_L] xK_Tab xK_grave)
+         ]
+
+main = xmonad gnomeConfig
+       {
+       modMask = mod4Mask
+       , layoutHook = smartBorders $ layoutHook defaultConfig
+       , manageHook = manageHook gnomeConfig <+> composeAll myManageHook
+       , keys = \c -> myKeys c `M.union` keys gnomeConfig c
+       }
