19d3800d7818c075604a6ee8d1a9a676a4f3502f
Arthur Ulfeldt <aulfeldt@hytrust.com>
2012-10-04 13:20:06 -0700

    add key for xfce terminal

--- xmonad.hs
+++ xmonad.hs
@@ -6,13 +6,13 @@ import XMonad.Actions.CycleRecentWS
 import XMonad.Layout.ShowWName
 import qualified Data.Map as M
 
-
 myManageHook :: [ManageHook]
 myManageHook = 
     [ resource  =? "Do"   --> doIgnore ]
 
 myKeys (XConfig {modMask = modm}) = M.fromList $
        	 [ ((modm, xK_p), spawn "gnome-do") 
+          ,((modm, xK_x), spawn "xfce4-terminal --hide-menubar")
           ,((modm, xK_Tab), cycleRecentWS [xK_Alt_L] xK_Tab xK_grave)
          ]
 
