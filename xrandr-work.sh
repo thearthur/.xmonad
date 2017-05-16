870c00fdafd7534cd37d8faf7a725c44bbc5f01e
Arthur Ulfeldt <arthur@ulfeldt.com>
2017-05-15 18:33:44 -0700

    first pass at three monitors on new xps 13

---
+++ xrandr-work.sh
@@ -0,0 +1,17 @@
+xrandr --output eDP1 \
+       --primary \
+       --mode 3200x1800 \
+       --pos 0x0 \
+       --rotate normal \
+
+xrandr --output DP1-2 \
+       --mode 1680x1050 \
+       --pos 5040x1800 \
+       --rotate left \
+       --scale 2x2
+
+xrandr --output DP1-3 \
+       --mode 1280x1024 \
+       --pos 3880x-500 \
+       --rotate normal \
+       --scale 2x2 \
